7c90f38541c32f4e1320b02e4c957f214539142b
Juraci Paixão Kröhling <juraci@kroehling.de>
2014-02-21 17:54:25 +0100

    Sample with KeyCloak

---
+++ src/main/java/org/sample/ejb/basic/JaxRsActivator.java
@@ -0,0 +1,8 @@
+package org.sample.ejb.basic;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1")
+public class JaxRsActivator extends Application {
+}
---
+++ src/main/java/org/sample/ejb/basic/SecuredEJB.java
@@ -0,0 +1,26 @@
+package org.sample.ejb.basic;
+
+import javax.annotation.Resource;
+import javax.annotation.security.RolesAllowed;
+import javax.ejb.SessionContext;
+import javax.ejb.Stateless;
+import java.security.Principal;
+
+@Stateless
+@RolesAllowed({ "user" })
+public class SecuredEJB {
+
+    // Inject the Session Context
+    @Resource
+    private SessionContext ctx;
+
+    /**
+     * Secured EJB method using security annotations
+     */
+    public String getSecurityInfo() {
+        // Session context injected using the resource annotation
+        Principal principal = ctx.getCallerPrincipal();
+
+        return principal.toString();
+    }
+}
